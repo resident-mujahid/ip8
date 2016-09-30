9c2239a6d6826db65742aedb1b19a470f81a3fa3
Stephen Emery <emery.stephen@gmail.com>
2016-09-30 11:07:50 -0700

    Add final junit tests for Animal.java

--- src/test/java/AnimalTest.java
+++ src/test/java/AnimalTest.java
@@ -45,30 +45,30 @@ public class AnimalTest {
     assertEquals(true, Animal.all().get(0).equals(testAnimal));
   }
 
-//   @Test
-//   public void save_assignsIdToAnimal() {
-//     Animal testAnimal = animal1;
-//     testAnimal.save();
-//     Animal savedAnimal = Animal.all().get(0);
-//     assertEquals(savedAnimal.getId(), testAnimal.getId());
-//   }
-//
-//   @Test
-//   public void all_returnsAllInstancesOfAnimal_true() {
-//     Animal firstAnimal = animal1;
-//     firstAnimal.save();
-//     Animal secondAnimal = animal2;
-//     secondAnimal.save();
-//     assertEquals(true, Animal.all().get(0).equals(firstAnimal));
-//     assertEquals(true, Animal.all().get(1).equals(secondAnimal));
-//   }
-//
-//   @Test
-//   public void find_returnsAnimalWithSameId_secondAnimal() {
-//     Animal firstAnimal = animal1;
-//     firstAnimal.save();
-//     Animal secondAnimal = animal2;
-//     secondAnimal.save();
-//     assertEquals(Animal.find(secondAnimal.getId()), secondAnimal);
-//   }
-// }
+  @Test
+  public void save_assignsIdToAnimal() {
+    Animal testAnimal = animal1;
+    testAnimal.save();
+    Animal savedAnimal = Animal.all().get(0);
+    assertEquals(savedAnimal.getId(), testAnimal.getId());
+  }
+
+  @Test
+  public void all_returnsAllInstancesOfAnimal_true() {
+    Animal firstAnimal = animal1;
+    firstAnimal.save();
+    Animal secondAnimal = animal2;
+    secondAnimal.save();
+    assertEquals(true, Animal.all().get(0).equals(firstAnimal));
+    assertEquals(true, Animal.all().get(1).equals(secondAnimal));
+  }
+
+  @Test
+  public void find_returnsAnimalWithSameId_secondAnimal() {
+    Animal firstAnimal = animal1;
+    firstAnimal.save();
+    Animal secondAnimal = animal2;
+    secondAnimal.save();
+    assertEquals(Animal.find(secondAnimal.getId()), secondAnimal);
+  }
+}
