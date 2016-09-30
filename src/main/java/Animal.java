6f336ad91ac61cede892e0fc490211e1689322fd
Stephen Emery <emery.stephen@gmail.com>
2016-09-30 11:02:04 -0700

    Add save, find, equals, and find methods to Animal.java.

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -23,5 +23,42 @@ public class Animal {
     return id;
   }
 
+  public void save() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "INSERT INTO animals (name, endangered) VALUES (:name, :endangered)";
+      this.id = (int) con.createQuery(sql, true)
+        .addParameter("name", this.name)
+        .addParameter("endangered", this.endangered)
+        .executeUpdate()
+        .getKey();
+    }
+  }
 
+  @Override
+  public boolean equals(Object otherAnimal) {
+    if (!(otherAnimal instanceof Animal)) {
+      return false;
+    } else {
+      Animal newAnimal = (Animal) otherAnimal;
+      return this.getName().equals(newAnimal.getName());
+    }
+  }
+
+  public static List<Animal> all() {
+    String sql = "select * from animals";
+    try(Connection con = DB.sql2o.open()) {
+      return con.createQuery(sql)
+      .executeAndFetch(Animal.class);
+    }
+  }
+
+  public static Animal find(int id) {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM animals WHERE id=:id";
+      Animal blog = con.createQuery(sql)
+        .addParameter("id", id)
+        .executeAndFetchFirst(Animal.class);
+      return blog;
+    }
+  }
 }
