440143a7bfbdba336f52a448212469efafb925ff
Stephen Emery <emery.stephen@gmail.com>
2016-09-30 11:12:05 -0700

    Minor updates to EndangeredTest.java

--- src/test/java/EndangeredAnimalTest.java
+++ src/test/java/EndangeredAnimalTest.java
@@ -7,38 +7,39 @@ import java.util.ArrayList;
 
 public class EndangeredAnimalTest {
 
-  EndangeredAnimal sighting1 = new EndangeredAnimal("Owl", "No", "ill", "young");
+  EndangeredAnimal endangered1 = new EndangeredAnimal("Spotted Owl", "Yes", "ill", "young");
+  EndangeredAnimal endangered2 = new EndangeredAnimal("Black Rhino", "Yes", "okay", "newborn");
 
   @Rule
   public DatabaseRule database = new DatabaseRule();
 
   @Test
   public void endangeredAnimal_instantiatesCorrectly_true() {
-    EndangeredAnimal testEndangeredAnimal = sighting1;
+    EndangeredAnimal testEndangeredAnimal = endangered1;
     assertEquals(true, testEndangeredAnimal instanceof EndangeredAnimal);
   }
 
   @Test
   public void getName_personInstantiatesWithName_Owl() {
-    EndangeredAnimal testEndangeredAnimal = sighting1;
-    assertEquals("Owl", testEndangeredAnimal.getName());
+    EndangeredAnimal testEndangeredAnimal = endangered1;
+    assertEquals("Spotted Owl", testEndangeredAnimal.getName());
   }
 
   @Test
   public void getEndangered_personInstantiatesWithEndangered_No() {
-    EndangeredAnimal testEndangeredAnimal = sighting1;
-    assertEquals("No", testEndangeredAnimal.getEndangered());
+    EndangeredAnimal testEndangeredAnimal = endangered1;
+    assertEquals("Yes", testEndangeredAnimal.getEndangered());
   }
 
   @Test
   public void getEndangered_personInstantiatesWithAge_young() {
-    EndangeredAnimal testEndangeredAnimal = sighting1;
+    EndangeredAnimal testEndangeredAnimal = endangered1;
     assertEquals("young", testEndangeredAnimal.getAge());
   }
 
   @Test
   public void getEndangered_personInstantiatesWithHealth_ill() {
-    EndangeredAnimal testEndangeredAnimal = sighting1;
+    EndangeredAnimal testEndangeredAnimal = endangered1;
     assertEquals("ill", testEndangeredAnimal.getHealth());
   }
 
